867766c5f6427bdbe66a1d0117a0b5062efcbb88
Eric <fantasy8me@126.com>
2021-08-29 22:51:34 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 radio.onReceivedString(function (receivedString) {
     item = receivedString
     if (item.compare("A") == 0) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 114)
         SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
     } else if (item.compare("B") == 0) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -114)
         SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
     } else if (item.compare("C") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    } else if (item.compare("D") == 0) {
         SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
         SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
-    } else if (item.compare("D") == 0) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 218)
     } else if (item.compare("0") == 0) {
         SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
         SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
@@ -21,13 +21,13 @@ radio.onReceivedString(function (receivedString) {
     } else if (item.compare("F") == 0) {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Green))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("F") == 0) {
+    } else if (item.compare("G") == 0) {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("G") == 0) {
+    } else if (item.compare("H") == 0) {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Yellow))
         SuperBit.RGB_Program().show()
-    } else if (item.compare("H") == 0) {
+    } else if (item.compare("I") == 0) {
         SuperBit.RGB_Program().clear()
         SuperBit.RGB_Program().show()
     }
