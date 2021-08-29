2ad7c109ae533c5624c18fb9dc443e79a271d3bf
Eric <fantasy8me@126.com>
2021-08-29 21:39:49 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,38 @@
-basic.forever(function () {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-    SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
-    basic.pause(2000)
+radio.onReceivedString(function (receivedString) {
+    item = receivedString
+    if (item.compare("A") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    } else if (item.compare("B") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+    } else if (item.compare("C") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+    } else if (item.compare("D") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 218)
+    } else if (item.compare("0") == 0) {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+    } else if (item.compare("E") == 0) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+        SuperBit.RGB_Program().show()
+    } else if (item.compare("F") == 0) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Green))
+        SuperBit.RGB_Program().show()
+    } else if (item.compare("F") == 0) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
+        SuperBit.RGB_Program().show()
+    } else if (item.compare("G") == 0) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Yellow))
+        SuperBit.RGB_Program().show()
+    } else if (item.compare("H") == 0) {
+        SuperBit.RGB_Program().clear()
+        SuperBit.RGB_Program().show()
+    }
 })
+let item = ""
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.showIcon(IconNames.SmallDiamond)
